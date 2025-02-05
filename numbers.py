dad38691f92bf08e5d1dd938f181717ce07909be
siva <2200080234@kluniversity.in>
2025-02-05 09:50:28 +0530

    Initial commit: Add numbers.py file

---
+++ numbers.py
@@ -0,0 +1,12 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Basic Python Program to Check Even or Odd
+... 
+... # Ask the user to input a number
+... number = int(input("Enter a number: "))
+... 
+... # Check if the number is even or odd
+... if number % 2 == 0:
+...     print(f"{number} is an Even number.")
+... else:
+...     print(f"{number} is an Odd number.")
